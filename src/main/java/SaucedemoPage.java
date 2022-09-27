de6797643bbf8b6fa895d862b992d055bcfa2229
PlotogonReal <plotogon@gmail.com>
2022-09-27 10:03:53 +0300

    Перенёс из теста некоторые строки в классы SaucedemoPage.java

--- src/main/java/SaucedemoPage.java
+++ src/main/java/SaucedemoPage.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.*;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class SaucedemoPage {
 
@@ -13,5 +16,14 @@ public class SaucedemoPage {
         $(By.id("user-name")).setValue(user);
         $(By.id("password")).setValue(password).pressEnter();
         return this;
+    }
+    public void checkTheCurrentPage(){
+        webdriver().shouldHave(url(baseUrl));
+    }
+    public void clickBurgerMenu(){
+        $(By.id("react-burger-menu-btn")).click();
+    }
+    public void clickLogout(){
+        $(By.id("logout_sidebar_link")).click();
     }
 }
--- src/test/java/LoginWithCorrectCredentials.java
+++ src/test/java/LoginWithCorrectCredentials.java
@@ -2,17 +2,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class LoginWithCorrectCredentials {
     static SaucedemoPage page = new SaucedemoPage();
 
     @BeforeAll
     public static void setup() {
-        //      Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://www.saucedemo.com/inventory.html";
         page.open();
     }
 
@@ -20,8 +16,8 @@ public class LoginWithCorrectCredentials {
     @ValueSource(strings = {"standard_user", "problem_user", "performance_glitch_user"})
     void login(String user) {
         page.login(user, "secret_sauce");
-        webdriver().shouldHave(url("https://www.saucedemo.com/inventory.html"));
-        $(By.id("react-burger-menu-btn")).click();
-        $(By.id("logout_sidebar_link")).click();
+        page.checkTheCurrentPage();
+        page.clickBurgerMenu();
+        page.clickLogout();
     }
 }
